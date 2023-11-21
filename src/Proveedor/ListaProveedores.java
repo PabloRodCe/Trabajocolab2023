f5d5c550a792a5e7e618ea3512b6da45fccdd70d
Pablo Rodriguez Centeno <jorodriguezce@est.utn.ac.cr>
2023-11-21 09:54:08 -0600

    se actualiza
    se realiza la documentacio del metodo buscar

--- src/Proveedor/ListaProveedores.java
+++ src/Proveedor/ListaProveedores.java
@@ -4,10 +4,39 @@
  */
 package Proveedor;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author Pablo Rod
  */
 public class ListaProveedores {
+    private ArrayList<Proveedor> lista ;
+    
+    public void agregar (Proveedor prov){
+    lista.add(prov);
+    
+    }
+    /**
+     * metodo que permite buscarun proveedores en el Arraylist
+     * @param codigo coigo del proveedor a buscar
+     * @return retorna el proveedor que conincide con el codigo o null
+     */
+    public Proveedor buscar(int codigo){
+        for (Proveedor prov : lista) {
+            if (prov.getCodigo()== codigo) {
+                return prov;
+            }
+            
+        }
+        return null;
+    }
+
+    public ArrayList<Proveedor> getLista() {
+        return lista;
+    }
+
+   
     
 }
